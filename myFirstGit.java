112e5ad5325949be8dc3af54aa7ede96f04fb294
Manidip Santra <manidips66@gmail.com>
2022-06-06 22:58:16 +0530

    initial stage

---
+++ myFirstGit.java
@@ -0,0 +1,6 @@
+public class myFirstGit {
+    public static void main(String[] args) {
+        String a = "Riki";
+        System.out.println(a);
+    }
+}
--- mylog.java
+++ mylog.java
@@ -3,5 +3,6 @@ public class mylog {
     public static void main(String[] args) {
         String name = "Riki";
         System.out.println(name);
+        // added comment
     }
 }
